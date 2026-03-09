ac4a08fce15d17410e4e95a02d7145508778ee84
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2026-03-09 13:09:04 +0100

    Added exporting a json file, containing scene data, to a given directory

---
+++ Assets/Scripts/Editor/HTMLConverter/SceneData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HTMLConverter
+{
+	[Serializable]
+	public struct SceneData
+	{
+		public CameraData Camera;
+		public List<CubeData> Cubes;
+	}
+
+	[Serializable]
+	public struct CameraData
+	{
+		public Vector3 Position;
+		public Quaternion Rotation;
+	}
+
+	[Serializable]
+	public struct CubeData
+	{
+		public int ParentIndex;
+		public Vector3 Position;
+		public Vector3 Rotation;
+		public Vector3 Scale;
+	}
+}
---
+++ Assets/Scripts/Editor/HTMLConverter/SceneExporter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace HTMLConverter
+{
+	public class SceneExporter
+	{
+		[MenuItem("Tools/Export To HTML5")]
+		public static void Export()
+		{
+			Debug.Log("Exporting");
+			string exportFolderPath = EditorUtility.OpenFolderPanel("Select path for HTML5 export", "", "");
+
+			if (string.IsNullOrEmpty(exportFolderPath))
+			{
+				return;
+			}
+
+			Debug.Log($"Selected folder: {exportFolderPath}");
+
+			SceneData sceneData = new SceneData();
+			Camera mainCamera = Camera.main;
+			sceneData.Camera = new CameraData()
+			{
+				Position = mainCamera.transform.position,
+				Rotation = mainCamera.transform.rotation
+			};
+
+			HTMLCube[] htmlCubes = GameObject.FindObjectsByType<HTMLCube>(FindObjectsSortMode.None);
+
+			sceneData.Cubes = new List<CubeData>();
+
+			foreach (HTMLCube cube in htmlCubes)
+			{
+				sceneData.Cubes.Add(new CubeData()
+				{
+					ParentIndex = -1,
+					Position = cube.transform.position,
+					Rotation = cube.transform.eulerAngles,
+					Scale = cube.transform.lossyScale
+				});
+			}
+
+			string sceneDataJson = JsonUtility.ToJson(sceneData, prettyPrint: true);
+
+			string filePath = Path.Combine(exportFolderPath, "scene.json");
+			File.WriteAllText(filePath, sceneDataJson);
+		}
+	}
+}
