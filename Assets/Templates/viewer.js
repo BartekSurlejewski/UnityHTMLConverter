883e157dc3d3f4373c7f85187b61dcd3d10a3645
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2026-03-12 14:04:40 +0100

    Changed template viewer.js to modify the camera's projection matrix to simulate left-handed coordinate system rendering. Code cleanup.

--- Assets/Templates/viewer.js
+++ Assets/Templates/viewer.js
@@ -1,3 +1,9 @@
+const CamProjectionMatrixModifier = new THREE.Matrix4().set // Used for changing the projection matrix to simulate left-handed coordinate system
+    (1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, -1, 0,
+        0, 0, 0, 1);
+
 function init() {
     const data = SCENE_DATA;
     const scene = new THREE.Scene();
@@ -14,29 +20,48 @@ function init() {
         1000
     );
 
-    const root = new THREE.Object3D();
-    root.name = "Scene Root";
-    root.position.set(0, 0, 0);
-    root.rotation.set(0, 0, 0);
-    root.scale.set(1, 1, -1);
-    scene.add(root);
+    initCamera(camera);
+    initSceneObjects(data, scene);
 
-    camera.position.set(
-        data.Camera.Position.x,
-        data.Camera.Position.y,
-        data.Camera.Position.z
-    );
-    camera.quaternion.set(
-        data.Camera.Rotation.x,
-        data.Camera.Rotation.y,
-        data.Camera.Rotation.z,
-        data.Camera.Rotation.w
-    );
-    camera.scale.set(1, 1, -1);
-    root.add(camera);
+    renderer.render(scene, camera);
+    window.addEventListener("resize", onWindowResize, false);
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+    }
+
+    function initCamera(camera) {
+        camera.position.set(
+            data.Camera.Position.x,
+            data.Camera.Position.y,
+            data.Camera.Position.z
+        );
+        camera.quaternion.set(
+            data.Camera.Rotation.x,
+            data.Camera.Rotation.y,
+            data.Camera.Rotation.z,
+            data.Camera.Rotation.w
+        );
+
+        const originalCamProjectionMatrixUpdate = camera.updateProjectionMatrix.bind(camera);
+        camera.updateProjectionMatrix = () => { // Override to make window resizing possible
+            originalCamProjectionMatrixUpdate();
+            camera.projectionMatrix.multiply(CamProjectionMatrixModifier);
+            camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert();
+        };
+
+        camera.updateProjectionMatrix();
+    }
+}
+
+function initSceneObjects(sceneData, scene) {
     const sceneObjects = [];
 
-    for (let c of data.SceneObjects) {
+    for (let c of sceneData.SceneObjects) {
 
         let sceneObject;
 
@@ -46,6 +71,7 @@ function init() {
         if (c.PrimitiveType == 1) {   // A cube
             const geo = new THREE.BoxGeometry(1, 1, 1);
             const mat = new THREE.MeshNormalMaterial();
+            mat.side = THREE.DoubleSide;
             sceneObject = new THREE.Mesh(geo, mat);
         }
 
@@ -63,24 +89,13 @@ function init() {
     }
 
     for (let i = 0; i < sceneObjects.length; i++) {
-        const parentIndex = data.SceneObjects[i].ParentIndex;
+        const parentIndex = sceneData.SceneObjects[i].ParentIndex;
 
         if (parentIndex !== -1) {
             sceneObjects[parentIndex].add(sceneObjects[i]); // add to parent object
         } else {
-            root.add(sceneObjects[i]);              // add to scene root
+            scene.add(sceneObjects[i]);              // add to scene root
         }
-    }
-
-    renderer.render(scene, camera);
-    window.addEventListener("resize", onWindowResize, false);
-
-    function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.render(scene, camera);
     }
 }
 
